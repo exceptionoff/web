a88201d056bdf99a6c85e5fd3abeff47cad4dccf
Nik <nikuvshinov@gmail.con>
2023-01-19 15:48:29 +0200

    no comment

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ln -sf /etc/nginx/sites-enabled/test.conf /home/box/web/etc/nginx.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
